c8af8f2df8ee614e3b6dbdfea9bda0568d67bffb
delgo123 <75376053+delgo123@users.noreply.github.com>
2022-05-06 15:21:00 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+//DOM VARIABLES
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
+//FUNCTIONS
 const setRotation = function (element, roationratio) {
     element.style.setProperty("--rotation", roationratio * 360);
 };
@@ -16,6 +18,7 @@ const setClock = function () {
     setRotation(hourHand, hoursRatio);
 };
 
+//FUNCTION CALLS
 setInterval(setClock, 1000);
 
 setClock();
